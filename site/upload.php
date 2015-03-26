5bd3cfef2b34ef4396be150d930883733a00cfc8
Filipe Rodrigues Pereira <filiperp@gmail.com>
2015-03-26 01:11:47 -0300

    vai

--- site/upload.php
+++ site/upload.php
@@ -1,9 +1,13 @@
 <?php
 
 
-$BASEPATH= 'C:\\Program Files (x86)\\Ampps\\www\\';
+$BASEPATH= 'C:\\\\Program Files (x86)\\\\Ampps\\\\www\\\\';
+$BASEPATH2= 'C:\\Program Files (x86)\\Ampps\\www\\';
 // A list of permitted file extensions
 $allowed = array('png', 'jpg', 'gif','zip', 'xlsx', 'xls', 'pdf');
+
+
+exec('taskkill /F /IM "excel.exe"',$superOut,$return_var);
 
 
 //var_dump ($_POST);
@@ -11,13 +15,13 @@ $allowed = array('png', 'jpg', 'gif','zip', 'xlsx', 'xls', 'pdf');
 
 $labName = $_POST['lablist'];
 $seller = $_POST['seller'];
-$date = $_POST['date'];
-$input = 'uploads\\'.$_FILES['upl']['name'];
+$date = str_pad( $_POST['day'], 2, "0", STR_PAD_LEFT). '/'.str_pad( $_POST['month'], 2, "0", STR_PAD_LEFT). '/'.$_POST['year'] ;
+$input = 'uploads\\\\'.$_FILES['upl']['name'];
 $fileNameWOExt = pathinfo($input,PATHINFO_FILENAME);
 $output = 'ready\\'.$labName.'_'.$seller.'_'.$fileNameWOExt.'.xlsx';
 
 
-$macroPath = $BASEPATH.'\\macros\\american_farma.vbs';
+$macroPath = $BASEPATH.'macros\\\\american_farma.vbs';
 //echo $labName;
 //die();
 
@@ -32,13 +36,17 @@ if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
 	}
 
 	if(move_uploaded_file($_FILES['upl']['tmp_name'], 'uploads/'.$labName.'_'.$seller.'_'.$_FILES['upl']['name'])){
+
+		$input = 'uploads\\\\'.$labName.'_'.$seller.'_'.$_FILES['upl']['name'];
         $ex = function_exists('exec');
 
-		$t = 'C:\\Debug\\MacroClia.exe  "'.$input.'"   "'.$output.'"  "'.$macroPath.'" "'.$seller.'" "'.$labName.'" "'.$date.'"  ' ;
+		$t = '.\\Debug\\MacroCli.exe  "'.$BASEPATH.$input.'"   "'.$BASEPATH2.$output.'"  "'.$macroPath.'" "'.$seller.'" "'.$labName.'" "'.$date.'"  ' ;
 		exec($t,$leoutput);
+
 		echo $leoutput;
 		echo '{"status":"success", "l": "'.$t.'", "ex":"'.$ex.'" }';
 		exit;
+
 	}
 
 }
@@ -46,4 +54,4 @@ if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
 echo '{"status":"error"}';
 exit;
 
-
+?>
